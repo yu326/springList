15a71f9caa4f13e69405d01012e682ac2c03dbec
Your Name <15652456348@163.com>
2017-07-27 19:12:54 +0800

    休整过后的书上实现链表的方法

--- src/main/java/yu/StandardLink.java
+++ src/main/java/yu/StandardLink.java
@@ -1,158 +1,158 @@
-//package yu;
-//
-//
-//import java.util.*;
-///**
-// * Created by koreyoshi on 2017/7/27.
-// */
-//public class StandardLink {
-//    private int theSize;
-//    private int modCount = 0;
-//    private Node<AnyType> beginMarker;
-//    private Node<AnyType> endMarker;
-//
-//    private static class Node<AnyType> {
-//
-//
-//        public Node(AnyType d, Node<AnyType> p, Node<AnyType> n) {
-//            data = d;
-//            prev = p;
-//            next = n;
-//        }
-//
-//        public AnyType data;
-//        public Node<AnyType> prev;
-//        public Node<AnyType> next;
-//    }
-//
-//    public StandardLink() {
-//        doClear();
-//    }
-//
-//    public void clear() {
-//        doClear();
-//    }
-//
-//    private void doClear() {
-//        beginMarker = new Node<AnyType>(null, null, null);
-//        endMarker = new Node<AnyType>(null, beginMarker, null);
-//        beginMarker.next = endMarker;
-//
-//
-//        theSize = 0;
-//        modCount++;
-//    }
-//
-//    public int size() {
-//        return theSize;
-//    }
-//
-//    public Boolean isEmpty() {
-//        return size() == 0;
-//    }
-//
-//    public Boolean add(AnyType x) {
-//        add(size(), x);
-//        return true;
-//    }
-//
-//    public void add(int idx, AnyType x) {
-//        addBefore(getNode(idx, 0, size()), x);
-//    }
-//
-//    public AnyType get(int idx) {
-//        return getNode(idx).data;
-//    }
-//
-//    public AnyType set(int idx, AnyType newVal) {
-//        Node<AnyType> p = getNode(idx);
-//        AnyType oldVal = p.data;
-//        p.data = newVal;
-//        return oldVal;
-//    }
-//
-//    public AnyType remove(int idx) {
-//        return remove(getNode(idx));
-//    }
-//
-//    private void addBefore(Node<AnyType> p, AnyType x) {
-//        Node<AnyType> newNode = new Node<>(x, p.prev, p);
-//        newNode.prev.next = newNode;
-//        p.prev = newNode;
-//        theSize++;
-//        modCount++;
-//    }
-//
-//    private AnyType remove(Node<AnyType> p) {
-//        p.next.prev = p.prev;
-//        p.prev.next = p.next;
-//        theSize--;
-//        modCount++;
-//        return p.data;
-//    }
-//
-//    private Node<AnyType> getNode(int idx) {
-//        return getNode(idx, 0, size() - 1);
-//    }
-//
-//    private Node<AnyType> getNode(int idx, int lower, int upper) {
-//        Node<AnyType> p;
-//        if (idx < lower || idx > upper) {
-//            throw new IndexOutOfBoundsException();
-//        }
-//        if (idx < size() / 2) {
-//            p = beginMarker.next;
-//            for (int i = 0; i < idx; i++) {
-//                p = p.next;
-//            }
-//        } else {
-//            p = endMarker;
-//            for (int i = size(); i > idx; i--) {
-//                p = p.prev;
-//            }
-//        }
-//        return p;
-//    }
-//
-//    public java.util.Iterator<AnyType> iterator() {
-//        return new LinkedListIterator();
-//    }
-//
-//    private class LinkedListIterator implements java.util.Iterator<AnyType> {
-//        private Node<AnyType> current = beginMarker.next;
-//        private int expectdModCount = modCount;
-//        private Boolean okToRemove = false;
-//
-//        public boolean hasNext() {
-//            return current != endMarker;
-//        }
-//
-//        public AnyType next() {
-//            if (modCount != expectdModCount) {
-//                throw new java.util.ConcurrentModificationException();
-//            }
-//            if (!hasNext()) {
-//                throw new java.util.NoSuchElementException();
-//            }
-//            AnyType nextItem = current.data;
-//            current = current.next;
-//            okToRemove = true;
-//            return nextItem;
-//        }
-//
-//        public void remove() {
-//            if (modCount != expectdModCount) {
-//                throw new java.util.ConcurrentModificationException();
-//            }
-//            if (!okToRemove) {
-//                throw new IllegalStateException();
-//            }
-//            StandardLink.this.remove(current.prev);
-//            expectdModCount++;
-//            okToRemove = false;
-//        }
-//
-//    }
-//
-//
-//}
+package yu;
+
+
+/**
+ * Created by koreyoshi on 2017/7/27.
+ */
+
+public class StandardLink<AnyType> implements Iterable<AnyType>{
+    private int theSize;
+    private int modCount = 0;
+    private Node<AnyType> beginMarker;
+    private Node<AnyType> endMarker;
+
+    private static class Node<AnyType> {
+
+
+        public Node(AnyType d, Node<AnyType> p, Node<AnyType> n) {
+            data = d;
+            prev = p;
+            next = n;
+        }
+
+        public AnyType data;
+        public Node<AnyType> prev;
+        public Node<AnyType> next;
+    }
+
+    public StandardLink() {
+        doClear();
+    }
+
+    public void clear() {
+        doClear();
+    }
+
+    private void doClear() {
+        beginMarker = new Node<AnyType>(null, null, null);
+        endMarker = new Node<AnyType>(null, beginMarker, null);
+        beginMarker.next = endMarker;
+
+
+        theSize = 0;
+        modCount++;
+    }
+
+    public int size() {
+        return theSize;
+    }
+
+    public Boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public Boolean add(AnyType x) {
+        add(size(), x);
+        return true;
+    }
+
+    public void add(int idx, AnyType x) {
+        addBefore(getNode(idx, 0, size()), x);
+    }
+
+    public AnyType get(int idx) {
+        return getNode(idx).data;
+    }
+
+    public AnyType set(int idx, AnyType newVal) {
+        Node<AnyType> p = getNode(idx);
+        AnyType oldVal = p.data;
+        p.data = newVal;
+        return oldVal;
+    }
+
+    public AnyType remove(int idx) {
+        return remove(getNode(idx));
+    }
+
+    private void addBefore(Node<AnyType> p, AnyType x) {
+        Node<AnyType> newNode = new Node<AnyType>(x, p.prev, p);
+        newNode.prev.next = newNode;
+        p.prev = newNode;
+        theSize++;
+        modCount++;
+    }
+
+    private AnyType remove(Node<AnyType> p) {
+        p.next.prev = p.prev;
+        p.prev.next = p.next;
+        theSize--;
+        modCount++;
+        return p.data;
+    }
+
+    private Node<AnyType> getNode(int idx) {
+        return getNode(idx, 0, size() - 1);
+    }
+
+    private Node<AnyType> getNode(int idx, int lower, int upper) {
+        Node<AnyType> p;
+        if (idx < lower || idx > upper) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (idx < size() / 2) {
+            p = beginMarker.next;
+            for (int i = 0; i < idx; i++) {
+                p = p.next;
+            }
+        } else {
+            p = endMarker;
+            for (int i = size(); i > idx; i--) {
+                p = p.prev;
+            }
+        }
+        return p;
+    }
+
+    public java.util.Iterator<AnyType> iterator() {
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements java.util.Iterator<AnyType> {
+        private Node<AnyType> current = beginMarker.next;
+        private int expectdModCount = modCount;
+        private Boolean okToRemove = false;
+
+        public boolean hasNext() {
+            return current != endMarker;
+        }
+
+        public AnyType next() {
+            if (modCount != expectdModCount) {
+                throw new java.util.ConcurrentModificationException();
+            }
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+            AnyType nextItem = current.data;
+            current = current.next;
+            okToRemove = true;
+            return nextItem;
+        }
+
+        public void remove() {
+            if (modCount != expectdModCount) {
+                throw new java.util.ConcurrentModificationException();
+            }
+            if (!okToRemove) {
+                throw new IllegalStateException();
+            }
+            StandardLink.this.remove(current.prev);
+            expectdModCount++;
+            okToRemove = false;
+        }
+
+    }
+
+
+}
