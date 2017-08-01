f3d63f7e537741e1561e7505562d2cda28b91d17
yu <koreyoshi326@126.com>
2017-08-01 08:42:15 +0800

    MyArrayList的remove修改

--- src/main/java/yu/MyTestArrayList.java
+++ src/main/java/yu/MyTestArrayList.java
@@ -3,8 +3,8 @@ package yu;
 /**
  * Created by koreyoshi on 2017/7/31.
  */
-public class MyTestArrayList<AnyType> {
-    //implements Iterable<AnyType>
+public class MyTestArrayList<AnyType> implements Iterable<AnyType> {
+    //
 
     //默认容器
     private static final int DEFAULT_CAPACITY = 10;
@@ -77,11 +77,36 @@ public class MyTestArrayList<AnyType> {
         for (int i = idx; i < size() - 1; i++) {
             theItems[i] = theItems[i + 1];
         }
-
+//        theSize--;
         theItems[--theSize] = null;
-
-        theSize--;
         return deleteItem;
+    }
+
+    public java.util.Iterator<AnyType> iterator(){
+        return new ArrayListIterator();
+    }
+
+    public class ArrayListIterator implements java.util.Iterator<AnyType> {
+        private int current = 0;
+
+        public boolean hasNext() {
+            return current < size();
+        }
+
+        public AnyType next(){
+            if(!hasNext())
+                throw new java.util.NoSuchElementException();
+
+            return theItems[current++];
+        }
+
+        public void remove(){
+            //MyArraList.this代表外层类MyArrayList的对象
+            //--current这里暂解释为remove在next方法后调用，next让current++,
+            //这里让current回到原来位置，删除它
+            //因为如果不调用next直接remove,current会出现<0情况
+            MyTestArrayList.this.remove(--current);
+        }
     }
 
 }
---
+++ src/main/java/yu/TestMyArrayList.java
@@ -0,0 +1,63 @@
+package yu;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by korey on 2017/7/30.
+ */
+public class TestMyArrayList {
+    public static void main(String[] args) {
+
+//        ArrayList a = new ArrayList();
+//        a.add(1);
+//        a.add(2);
+//        a.add(3);
+//        a.remove(1);
+//        System.out.println(a);
+
+
+
+        MyTestArrayList mal = new MyTestArrayList();
+
+        mal.add(1);
+        mal.add("2");
+        mal.add(3);
+        mal.add("4");
+        mal.add("5");
+        mal.add("6");
+        mal.add("7");
+        mal.add("8");
+        mal.add("9");
+        mal.add("10");
+        mal.add("11");
+        mal.add("12");
+        mal.remove(3);
+        System.out.println(mal);
+//        System.out.println(mal.remove(3));
+//        System.out.println(mal);
+
+//        for(java.util.Iterator<String> its=mal.iterator();its.hasNext();){
+//            System.out.println(its.next());
+//            its.remove();
+//            //这里要看到元素，得自己实现toString方法然后调用！
+//            System.out.println(mal);
+//        }
+        for (Iterator its = mal.iterator(); its.hasNext(); ) {
+            System.out.println(its.next());
+            its.remove();
+
+        }
+
+
+//        for (java.util.Iterator<String> its =mal.iterator();its.hasNext();){
+//            String tmp = its.next();
+//            System.out.println(tmp);
+//            if(tmp == "3"){
+//                its.remove();
+//            }
+//
+//        }
+
+    }
+}
