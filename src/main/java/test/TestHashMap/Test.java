d8db54f9fad0ada457aa83e2f5e69b82d0570674
yu <koreyoshi326@126.com>
2017-09-27 00:06:44 +0800

    jaonObject循环及Iteator 迭代器

--- src/main/java/test/TestHashMap/Test.java
+++ src/main/java/test/TestHashMap/Test.java
@@ -1,7 +1,8 @@
 package test.TestHashMap;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.*;
 
 /**
  * Created by koreyoshi on 2017/9/15.
@@ -9,17 +10,44 @@ import java.util.HashSet;
 public class Test {
 
     public static void main(String[] args) {
-        HashMap<String, Double> map = new HashMap<String, Double>();
+//        HashMap<String, Double> map = new HashMap<String, Double>();
+//
+//        map.put("语文", 80.0);
+//        map.put("数学", 89.0);
+//        map.put("英语", 78.2);
+//        System.out.println(map);
+//
+//
+//        HashSet<Object> set = new HashSet<Object>();
+//        set.add("123");
+//        System.out.println(set);
 
-        map.put("语文", 80.0);
-        map.put("数学", 89.0);
-        map.put("英语", 78.2);
-        System.out.println(map);
+        JSONObject data = new JSONObject(3);
+        data.put("suixin","yu");
+        data.put("suiyuan","yi");
+        data.put("yu","love");
 
 
-        HashSet<Object> set = new HashSet<Object>();
-        set.add("123");
-        System.out.println(set);
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
 
+
+        Map<String,Object> map = new HashMap<String, Object>();
+        map.put("suixin","yu");
+        map.put("suiyuan","yi");
+        map.put("yu","love");
+        for (Map.Entry<String,Object> entry : map.entrySet())
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        List<Object> list = new ArrayList<Object>(3);
+        list.add("yu");
+        list.add("yu1");
+        list.add("yu2");
+
+        Iterator it = list.iterator();
+        while(it.hasNext()) {
+                 Object obj = it.next();
+                 System.out.println(obj);
+        }
     }
 }
